a750f70c181f765f3d3c49bbe0281086f372a44a
esteban7700 <jhordy.salinas@mail.escuelaing.edu.co>
2017-11-01 15:56:57 -0500

    solo mio

--- src/main/java/edu/eci/arsw/collabhangman/WebSocketConfig.java
+++ src/main/java/edu/eci/arsw/collabhangman/WebSocketConfig.java
@@ -32,13 +32,13 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        config.enableStompBrokerRelay("/topic/").setRelayHost("192.168.56.102").setRelayPort(61613);
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stompendpoint").withSockJS();
+        registry.addEndpoint("/stompendpoint").setAllowedOrigins("*").withSockJS();
     }
 
 }
