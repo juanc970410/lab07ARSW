942070688041099b0fc0dd673f9205f95d5e662b
esteban7700 <jhordy.salinas@mail.escuelaing.edu.co>
2017-11-01 16:57:02 -0500

    agregando aplication.properties para no quemar los datos y otras cosas

--- src/main/java/edu/eci/arsw/collabhangman/WebSocketConfig.java
+++ src/main/java/edu/eci/arsw/collabhangman/WebSocketConfig.java
@@ -20,6 +20,7 @@ package edu.eci.arsw.collabhangman;
  *
  * @author hcadavid
  */
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
@@ -29,10 +30,15 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+    @Value("${port}")
+    private int port;
+    @Value("${host}")
+    private String host;
+           
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableStompBrokerRelay("/topic/").setRelayHost("192.168.56.102").setRelayPort(61613);
+        config.enableStompBrokerRelay("/topic/").setRelayHost(host).setRelayPort(port);
         config.setApplicationDestinationPrefixes("/app");
     }
 
